42ec8612b19c981f638c7732b669b75d3935d029
Sunil Kumar Katta <111487330+SunilKumar-K@users.noreply.github.com>
2023-08-10 12:52:58 +0000

    added react learnign project 2 form scrimba

---
+++ reactFormDependencies/project_2/index.js
@@ -0,0 +1,17 @@
+const page = (
+        <div>
+            <h1>
+                Reasons I'm excited to learn react.
+            </h1>
+
+            <ol>
+                <li>
+                    It is simple to learn.
+                </li>
+            </ol>
+        </div>
+    )
+
+
+
+ReactDOM.render(page,document.getElementById("root"))
